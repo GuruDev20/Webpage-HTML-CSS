bc81e2a8eebf5b3606e84a9d2c380c1d7fce56b6
Guru Dev <guru01803@gmail.com>
2023-10-16 11:21:08 +0530

    updated

---
+++ practice-full-stack/src/components/GoalList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GoalList.css';
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Dev</li>
+      <li>Divya</li>
+      <li>Raj</li>
+    </ul>
+  );
+};
+export default GoalList;
