1238ec7462b1a1cb75606380a0e4d23c17a2c84e
Guru Dev <guru01803@gmail.com>
2023-10-16 12:48:00 +0530

    updated

--- practice-full-stack/src/App.js
+++ practice-full-stack/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/Newgoal';
 import './App.css';
 const App=()=>{
+  const courseGoals=[
+    {id:'cg1',text:'Dev'},
+    {id:'cg2',text:'Divya'},
+    {id:'cg3',text:'Raj'}
+  ];
   return(
     <div className='user'>
       <h2>Users</h2>
-      <GoalList/>
+      <NewGoal />
+      <GoalList goals={courseGoals}/>
     </div>
   );
 };
